fe710bf02a7542a69b8203b883468376abd44c36
Penguuuuu <pengugithub@gmail.com>
2025-04-29 03:35:03 +1000

    Small cleanup

--- public/roundtext.js
+++ public/roundtext.js
@@ -1,11 +1,16 @@
 const roundToPixels = () => {
     document.querySelectorAll('*').forEach(el => {
         if (el.textContent.trim() !== '') {
+            el.style.marginLeft = '0px';
+            el.style.marginTop = '0px';
             const rect = el.getBoundingClientRect();
             const [dx, dy] = [Math.round(rect.left) - rect.left, Math.round(rect.top) - rect.top];
-            if (Math.abs(dx) > 0.01 || Math.abs(dy) > 0.01) {
-                const transform = getComputedStyle(el).transform.replace(/translate\([^)]+\)/, '').replace('none', '');
-                el.style.transform = `${transform} translate(${dx}px, ${dy}px)`.trim();
+            if (Math.abs(dx) > 0 || Math.abs(dy) > 0) {
+                el.style.marginLeft = `${dx}px`;
+                el.style.marginTop = `${dy}px`;
+            } else {
+                el.style.marginLeft = '';
+                el.style.marginTop = '';
             }
         }
     });
